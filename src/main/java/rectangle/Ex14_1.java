5318dbedd1a94ec4553629cb38d1df4988d57990
HaTien <havantien95tb@gmail.com>
2018-08-29 09:03:18 +0700

    Tao class chua phuong thuc main de thuc thi chuong trinh

---
+++ src/main/java/rectangle/Ex14_1.java
@@ -0,0 +1,17 @@
+package rectangle;
+
+import java.util.Scanner;
+
+public class Ex14_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the width:");
+        double width = sc.nextDouble();
+        System.out.print("Enter the height:");
+        double height = sc.nextDouble();
+        RectangleThucHanh rectangle = new RectangleThucHanh(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
