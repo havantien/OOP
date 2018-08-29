8f8c58c3e4e2ef3b77b50ae246ce1ab2571b1282
HaTien <havantien95tb@gmail.com>
2018-08-29 09:02:43 +0700

    Tao lop rectangle khai bao thuoc tinh, dinh nghia cac lop khoi tao

---
+++ src/main/java/rectangle/RectangleThucHanh.java
@@ -0,0 +1,29 @@
+package rectangle;
+
+import java.util.Scanner;
+
+public class RectangleThucHanh {
+    double width, height;
+
+    public RectangleThucHanh(){
+
+    }
+
+    public RectangleThucHanh(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+
+}
